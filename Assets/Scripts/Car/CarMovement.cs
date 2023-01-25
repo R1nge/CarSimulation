f9c53138982c8d0d67366a1b2ae4afc087f25983
R1ngeDev <r1ngegamedev@gmail.com>
2023-01-25 17:48:39 +0300

    WheelsController

--- Assets/Scripts/Car/CarMovement.cs
+++ Assets/Scripts/Car/CarMovement.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Car
 {
@@ -8,43 +7,45 @@ namespace Car
         [SerializeField] private float maxSpeed;
         [SerializeField] private float accelerationTime;
         [SerializeField] private float distanceToGround;
+        private WheelsController _wheelsController;
         private Vector3 _moveDirection;
         private float _time;
         private Rigidbody _rigidbody;
 
         private void Awake()
         {
+            _wheelsController = GetComponent<WheelsController>();
             _rigidbody = GetComponent<Rigidbody>();
         }
 
-        private void Update()
-        {
-            if (IsGrounded())
-            {
-                GetInput();
-            }
-            else
-            {
-                ResetTime();
-            }
-
-            IncreaseTime();
-        }
-
-        private void FixedUpdate()
-        {
-            Move();
-        }
+        // private void Update()
+        // {
+        //     IncreaseTime();
+        // }
+        //
+        // private void FixedUpdate()
+        // {
+        //     if (IsGrounded())
+        //     {
+        //         GetInput();
+        //     }
+        //     else
+        //     {
+        //         ResetTime();
+        //     }
+        //
+        //     
+        // }
 
         private void GetInput()
         {
             if (Input.GetKey(KeyCode.W))
             {
-                _moveDirection = transform.forward;
+                Move(transform.forward);
             }
             else if (Input.GetKey(KeyCode.S))
             {
-                _moveDirection = -transform.forward;
+                Move(-transform.forward);
             }
             else if (Input.GetKey(KeyCode.A))
             {
@@ -77,10 +78,11 @@ namespace Car
             _rigidbody.AddForce(-_moveDirection * LerpSpeed(_time / accelerationTime, 0));
         }
 
-        private void Move()
+        private void Move(Vector3 dir)
         {
             if (_rigidbody.velocity.magnitude >= maxSpeed) return;
-            _rigidbody.AddForce(_moveDirection * LerpSpeed(0, maxSpeed));
+            _rigidbody.AddForce(dir * (LerpSpeed(0, maxSpeed) * _rigidbody.mass));
+            _moveDirection = dir;
         }
 
         private float LerpSpeed(float from, float to) => Mathf.Lerp(from, to, _time / accelerationTime);
---
+++ Assets/Scripts/Car/WheelsController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+namespace Car
+{
+    public class WheelsController : MonoBehaviour
+    {
+        [SerializeField] private float motorForce = 50;
+        [SerializeField] private float maxSteerAngle = 30;
+        [SerializeField] private Transform[] frontWheels, backWheels;
+        [SerializeField] private WheelCollider[] frontWheelsColliders, backWheelColliders;
+        private float _horizontalInput;
+        private float _verticalInput;
+        private float _steeringAngle;
+
+
+        private void FixedUpdate()
+        {
+            GetInput();
+            Steer();
+            Accelerate();
+            UpdateWheelPoses();
+        }
+
+        private void GetInput()
+        {
+            _horizontalInput = Input.GetAxis("Horizontal");
+            _verticalInput = Input.GetAxis("Vertical");
+        }
+
+        private void Steer()
+        {
+            _steeringAngle = maxSteerAngle * _horizontalInput;
+            for (int i = 0; i < frontWheels.Length; i++)
+            {
+                frontWheelsColliders[i].steerAngle = _steeringAngle;
+            }
+        }
+
+        private void Accelerate()
+        {
+            for (int i = 0; i < backWheelColliders.Length; i++)
+            {
+                backWheelColliders[i].motorTorque = _verticalInput * motorForce;
+            }
+        }
+
+        private void UpdateWheelPoses()
+        {
+            for (int i = 0; i < frontWheels.Length; i++)
+            {
+                UpdateWheelPose(frontWheels[i], frontWheelsColliders[i]);
+            }
+
+            for (int i = 0; i < backWheels.Length; i++)
+            {
+                UpdateWheelPose(frontWheels[i], frontWheelsColliders[i]);
+            }
+        }
+
+        private void UpdateWheelPose(Transform transform, WheelCollider collider)
+        {
+            collider.GetWorldPose(out var pos, out var quat);
+            transform.position = pos;
+            transform.rotation = quat;
+        }
+    }
+}
